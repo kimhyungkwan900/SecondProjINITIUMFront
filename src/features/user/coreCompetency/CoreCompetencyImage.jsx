2d0afd9076615da1846df711ca293260155d5df4
jiwooa <wldn5913@gmail.com>
2025-08-04 18:28:07 +0900

    임시저장

---
+++ src/features/user/coreCompetency/CoreCompetencyImage.jsx
@@ -0,0 +1,10 @@
+
+
+const CoreCompetencyImage = () =>{
+    return(
+        <div>
+            <h1>추후 이미지 삽입</h1>
+        </div>
+    );
+};
+export default CoreCompetencyImage;
--- src/router/createAppRouter.jsx
+++ src/router/createAppRouter.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import loginRouter from "./user/auth/loginRouter";
 import MainPage from "../pages/MainPage";
+import coreCompetencyRouter from "./user/coreCompetency/coreCompetencyRouter";
 
 const createAppRouter = () => {
     return createBrowserRouter([
         ...loginRouter,
+        ...coreCompetencyRouter,
         {
             path: "/",
             element: <MainPage />
