0b38f6bd32d08e8e6a7d97cf0955c6ac05aa7026
kim Gun ho <vlfh0002@gmail.com>
2025-08-04 19:41:24 +0900

    0804 commit

--- src/router/createAppRouter.jsx
+++ src/router/createAppRouter.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import loginRouter from "./user/auth/loginRouter";
 import MainPage from "../pages/MainPage";
-
+import ExtracurricularRoutes from "./admin/extracurricular/ExtracurricularRouter";
 const createAppRouter = () => {
     return createBrowserRouter([
         ...loginRouter,
+        ...ExtracurricularRoutes,
         {
             path: "/",
             element: <MainPage />
