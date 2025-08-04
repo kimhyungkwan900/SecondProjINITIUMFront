5c472d5a3c791299f02ebded0ea2609949d23403
jiwooa <wldn5913@gmail.com>
2025-08-04 18:28:07 +0900

    임시저장

--- src/router/createAppRouter.jsx
+++ src/router/createAppRouter.jsx
@@ -3,12 +3,14 @@ import loginRouter from "./user/auth/loginRouter";
 import MainPage from "../pages/MainPage";
 import ExtracurricularRoutes from "./admin/extracurricular/ExtracurricularRouter";
 import { diagnosticRoutes } from "./user/diagnostic/diagnosticRouter";
+import coreCompetencyRouter from "./user/coreCompetency/coreCompetencyRouter";
 
 const createAppRouter = () => {
     return createBrowserRouter([
         ...loginRouter,
         ...ExtracurricularRoutes,
         ...diagnosticRoutes,
+        ...coreCompetencyRouter,
         {
             path: "/",
             element: <MainPage />
