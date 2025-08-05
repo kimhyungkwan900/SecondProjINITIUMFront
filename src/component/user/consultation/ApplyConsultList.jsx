e7390aafa0a8a943b9892fa773555a05c9ab349b
shchae <snghynchae@naver.com>
2025-08-05 18:13:46 +0900

    2025-08-05 1813

--- src/component/user/consultation/ApplyConsultList.jsx
+++ src/component/user/consultation/ApplyConsultList.jsx
@@ -1,7 +1,10 @@
 const ApplyConsultList = ()=>{
     return(
-        <div>
-
+        <div className="flex flex-col fixed border border-black z-[1] top-[200px] left-[280px] w-4/5">
+            <div>목록1</div>
+            <div>목록2</div>
+            <div>목록3</div>
+            <div>목록4</div>
         </div>
     );
 }
