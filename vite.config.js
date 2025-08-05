c2974a901ba36b964219d33779490435652ffa00
김형관 <bearson0115@gmail.com>
2025-08-05 17:54:58 +0900

    2025_08_05_1755_김형관

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-       '/api': {
+      '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
-        cookieDomainRewrite: 'localhost'
+        rewrite: (path) => {
+          console.log('Proxying request:', path)
+          return path
+        }
       },
-
     },
   },
 })
