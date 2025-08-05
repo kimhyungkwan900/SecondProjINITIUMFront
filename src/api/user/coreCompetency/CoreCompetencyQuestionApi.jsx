9d61921474a14935ac6acfe61522db5cd0ac38da
jiwooa <wldn5913@gmail.com>
2025-08-05 16:29:04 +0900

    임시저장

---
+++ src/api/user/coreCompetency/CoreCompetencyQuestionApi.jsx
@@ -0,0 +1,12 @@
+
+import axios from "axios";
+
+// ✅ axios 공통 인스턴스 생성 (쿠키 자동 포함 + baseURL)
+const axiosInstance = axios.create({
+  withCredentials: true,
+});
+
+// 핵심역량진단목록 전체 불러오기
+export const getAllAssessments = () => {
+  return axiosInstance.post(`/api/admin/core-competency/question/get/all`);
+};
---
+++ src/component/user/coreCompetency/CoreCompetencyResult.jsx
@@ -0,0 +1,14 @@
+import CoreCompetencySideBar from "../../../features/user/coreCompetency/CoreCompetencySideBar";
+import MainHeader from "../../../features/user/mainpage/MainHeader";
+
+const CoreCompetencyResult = () =>{
+    return(
+        <div>
+            <MainHeader/>
+            <CoreCompetencySideBar/>
+            진단결과페이지
+        </div>
+    );
+};
+
+export default CoreCompetencyResult;
