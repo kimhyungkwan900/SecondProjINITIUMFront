8f63596cfc3ed0726fb078e3156627f8350a724e
김형관 <bearson0115@gmail.com>
2025-08-06 10:06:05 +0900

    2025_08_06_1006_김형관

--- src/features/user/mainpage/MainHeader.jsx
+++ src/features/user/mainpage/MainHeader.jsx
@@ -8,8 +8,8 @@ export default function MainHeader() {
         {
             label: "진단검사",
             submenu: [
-                {label: "진단검사", to: "/diagnosis-home"},
-                {label: "내부진단검사", to: "/diagnosis"},
+                {label: "진단검사", to: "/diagnosis"},
+                {label: "내부진단검사", to: "/diagnosis/internal"},
                 {label: "외부진단검사", to: "/external-diagnosis"},
             ],
         },
