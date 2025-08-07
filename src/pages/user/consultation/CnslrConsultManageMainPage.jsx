93925eb0a834f50d22125b3c6498207e81005061
shchae <snghynchae@naver.com>
2025-08-07 18:01:22 +0900

    2025-08-07 1801

--- src/pages/user/consultation/CnslrConsultManageMainPage.jsx
+++ src/pages/user/consultation/CnslrConsultManageMainPage.jsx
@@ -60,13 +60,9 @@ const CnslrConsultManageMainPage = ()=>{
 
                 {/* 우측 본문 콘텐츠 */}
                 <div className="flex-1 flex-col ml-10">
-                    {applyItems.map((item)=>{
-                        <ApplyConsultBox info={item}/>
-                    })}
-                    {/* <ApplyConsultBox imgSrc={''} name={'지도교수 상담'} description={'교수님과 상담을 원하는 학생은 상담을 신청해주세요.'} type={'상담신청'}/>
-                    <ApplyConsultBox name={'진로취업 상담'} description={'진로에 고민이 있거나 취업정보가 부족해 피드백이 필요한 학생은 상담을 신청해주세요.'} type={'상담신청'}/>
-                    <ApplyConsultBox name={'심리상담'} description={'심리적 어려움이나 고민이 있는 학생은 상담을 신청해주세요.'} type={'상담신청'}/>
-                    <ApplyConsultBox name={'학습상담'} description={'학습 방법이나 성적 향상에 대해 상담이 필요한 학생은 신청해주세요.'} type={'상담신청'}/>*/}
+                    {applyItems.map((item, idx)=>(
+                        <ApplyConsultBox key={idx} info={item}/>
+                    ))}
                 </div>
             </div>
         </div>
