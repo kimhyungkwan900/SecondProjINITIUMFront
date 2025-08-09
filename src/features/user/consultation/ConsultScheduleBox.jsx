0fda0544d65b4177c76c4f25ba51d5d82acb9ce7
shchae <snghynchae@naver.com>
2025-08-09 11:42:53 +0900

    2025-08-09 1142

--- src/features/user/consultation/ConsultScheduleBox.jsx
+++ src/features/user/consultation/ConsultScheduleBox.jsx
@@ -1,10 +1,6 @@
 const ScheduleBox = ()=>{
     return(
-        <div className="w-full flex flex-col bg-blue-600 text-white rounded px-2 py-3 text-sm">
-            <div className="mb-2">
-                <span className="font-medium">9:00</span>
-                <span className="ml-2">예약 대기</span>
-            </div>
+        <div className="w-full flex flex-col bg-blue-600 text-white rounded px-3 py-3 text-sm">
             <select className="w-2/3 justify-end text-black">
                 <option>테스트1</option>
                 <option>테스트2</option>
